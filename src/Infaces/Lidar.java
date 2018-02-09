bc31a15792fc444106e34337772d62b884389fa4
MHLtd <mehrhosse@gmail.com>
2018-02-09 12:23:02 +0100

    Radar class Lidar class

---
+++ src/Infaces/Lidar.java
@@ -0,0 +1,18 @@
+package Infaces;
+
+public class Lidar {
+    private int sensorValue;
+
+    public Lidar(int sensorValue) {
+        this.sensorValue = sensorValue;
+    }
+
+    public void setSensorValue(int sensorValue) {
+        this.sensorValue = sensorValue;
+    }
+
+    public int getSensorValue() {
+
+        return sensorValue;
+    }
+}
---
+++ src/Infaces/Radar.java
@@ -0,0 +1,18 @@
+package Infaces;
+
+public class Radar {
+    private int sensorValue;
+
+    public Radar(int sensorValue) {
+        this.sensorValue = sensorValue;
+    }
+
+    public void setSensorValue(int sensorValue) {
+        this.sensorValue = sensorValue;
+    }
+
+    public int getSensorValue() {
+
+        return sensorValue;
+    }
+}
