9f0ede4b8f566de05ca690d607cdd7840582bd7d
MHLtd <mehrhosse@gmail.com>
2018-02-19 19:32:05 +0100

    ref

--- src/vehicle/model/Actuator.java
+++ src/vehicle/model/Actuator.java
@@ -2,5 +2,5 @@ package vehicle.model;
 
 public interface Actuator {
 
-    public int moveVehicle(int[] vehicleCoordinates, int distance);
+     int moveVehicle(int[] vehicleCoordinates, int distance);
 }
